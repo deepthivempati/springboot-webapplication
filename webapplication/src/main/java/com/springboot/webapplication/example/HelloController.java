d8b9b1c3c82cda0b0e7b280bbb945afcf78e88dc
deepthivempati <deepthi2135@gmail.com>
2022-12-12 00:28:14 +0530

    ,

--- webapplication/src/main/java/com/springboot/webapplication/example/HelloController.java
+++ webapplication/src/main/java/com/springboot/webapplication/example/HelloController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -16,7 +17,8 @@ public class HelloController {
         System.out.println(hello);
     }
     @GetMapping("/webapp")
+   // @ResponseBody  // when this is used return statement will be the output(hello)
     public String sayHello() {
-        return "hello";
+        return "hello"; //we are not using @Responsebody so our request is a view--> application.yml file is configured with few properties called prefix and sufix which calls the respective view(calls the hello jsp file)
     }
 }
